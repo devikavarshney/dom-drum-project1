7f4955615512186a99d078f0fc5df11a04415afb
Devika0003 <74262916+devikavarshney@users.noreply.github.com>
2021-09-23 17:09:49 +0530

    Delete Drum Kit Starting Files directory

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++
@@ -1,7 +0,0 @@
-var drum = document.querySelectorAll(".drum");
-for (var i = 0; i < drum.length; i++) {
-  drum[i].addEventListener("click", function() {
-    var audio = new Audio('.\sounds\snare.mp3');
-    audio.play();
-  });
-}
